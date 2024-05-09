a3d9d223957b65540e7affe2f3371a71fb044965
Tom Wilkins <tom.wilkins@citrix.com>
2024-05-09 16:23:17 +0100

    Updating copyright notice

--- Citrix.Unified.Api.Test.WebClient/Pages/Index.cshtml.cs
+++ Citrix.Unified.Api.Test.WebClient/Pages/Index.cshtml.cs
@@ -1,4 +1,8 @@
-﻿// Copyright © 2023. Cloud Software Group, Inc. All Rights Reserved.
+﻿/*
+* Copyright © 2023. Cloud Software Group, Inc.
+* This file is subject to the license terms contained
+* in the license file that is distributed with this file.
+*/
 
 using Citrix.Unified.Api.Test.WebClient.CitrixOidc;
 using Citrix.Unified.Api.Test.WebClient.Resources;
